d76c9e0a4ddc695f6bea149ad65a1f1abc87ef46
Tucker Miller <tdmiller7@bsu.edu>
2017-09-19 16:24:36 -0400

    JsonParsing Edit

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -8,20 +8,26 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.Map;
 
-public class jsonParser {
+public class Parser {
 
+    JsonArray array = null;
     public void parseJsonFile(){
-        JsonParser parser = new JsonParser();
+        com.google.gson.JsonParser parser = new com.google.gson.JsonParser();
         InputStream inputStream = getClass().getClassLoader().getResourceAsStream("sample.json");
         Reader reader = new InputStreamReader(inputStream);
         JsonElement rootElement = parser.parse(reader);
         JsonObject rootObject = rootElement.getAsJsonObject();
         JsonObject pages = rootObject.getAsJsonObject("query").getAsJsonObject("pages");
-        JsonArray array = null;
         for(Map.Entry<String,JsonElement> entry : pages.entrySet()){
             JsonObject entryObject = entry.getValue().getAsJsonObject();
             array = entryObject.getAsJsonArray("revisions");
         }
+    }
 
+    public boolean isEmpty(){
+        if(array == null){
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/URLConnectionReaderTest.java
+++ src/test/java/URLConnectionReaderTest.java
@@ -1,7 +1,36 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Map;
+
 public class URLConnectionReaderTest {
 
+    //Needs Assert
+    @Test
+    public void testURLNotNull() throws Exception {
+        URLConnectionReader reader = new URLConnectionReader();
+        reader.searchWikipediaTitle("Soup", "4");
+        Parser jsonObject = new Parser();
+        jsonObject.parseJsonFile();
+        boolean result = jsonObject.isEmpty();
+        //Assert.assertEquals(false, result);
+    }
 
+
+
+    @Test
+    public void testURLNull() throws Exception {
+        URLConnectionReader reader = new URLConnectionReader();
+        reader.searchWikipediaTitle("", "4");
+        Parser jsonObject = new Parser();
+        jsonObject.parseJsonFile();
+        boolean result = jsonObject.isEmpty();
+        Assert.assertEquals(true, result);
+    }
 }
